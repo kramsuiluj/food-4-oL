7e197036300ccdbe38db3ab5ae41c6b9f0eab4fd
kramsuiluj <mjmaravillo@gmail.com>
2021-03-27 22:31:34 +0800

    added header content with initial styles

--- includes/header.php
+++ includes/header.php
@@ -4,5 +4,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>food-4-oL | Zero-Hunger Movement</title>
+    <style>
+        <?php require_once 'styles.css'; ?>
+    </style>
 </head>
 <body>
+
+<header>
+    <h1>food-4-oL</h1>
+    <div>
+        <a href="#">Sign Up</a>
+        <a href="#">Login</a>
+    </div>
+</header>
