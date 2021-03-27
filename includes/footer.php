78045bfe4e3af48e4232711bb471eb9f58d8f237
kramsuiluj <mjmaravillo@gmail.com>
2021-03-27 22:46:37 +0800

    added footer content with initial styles

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,11 @@
-
+<footer>
+    <p>© 2021 food-4-oL | Zero-Hunger Movement</p>
+    <p>
+        <span><a href="#">About Us</a></span>
+        ●
+        <span><a href="#">Contact</a></span>
+    </p>
+</footer>
     
 </body>
 </html>
