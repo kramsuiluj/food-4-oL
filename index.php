f7fd3f8b1b910308980e0af797d0f745ca7252a7
kramsuiluj <mjmaravillo@gmail.com>
2021-03-27 22:51:34 +0800

    added main section in index.php file

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php require_once 'includes/header.php'; ?>
 
+<main>
 
+    <section class="intro">
+        <h1>Join us on our mission!</h1>
+        <p>Food-4-oL (Zero-Hunger Movement) website encourages us to help others amid the turmoil we are experiencing, executing avenue so that it is easier for us to contribute with just a click.</p>
+    </section>
+
+    <section class="logo">
+
+    </section>
+
+</main>
 
 <?php require_once 'includes/footer.php'; ?>
