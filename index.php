45db58170b6ea121d5250e7217d541564df655b0
kramsuiluj <mjmaravillo@gmail.com>
2021-03-27 23:31:21 +0800

    added main content in index.php with initial styles

--- index.php
+++ index.php
@@ -4,11 +4,20 @@
 
     <section class="intro">
         <h1>Join us on our mission!</h1>
+        <br>
         <p>Food-4-oL (Zero-Hunger Movement) website encourages us to help others amid the turmoil we are experiencing, executing avenue so that it is easier for us to contribute with just a click.</p>
+        <br>
+        <br>
+        <p>
+            <span>Learn How It Works</span>
+            <span>
+                <a href="#">Get Started</a>
+            </span>
+        </p>
     </section>
 
     <section class="logo">
-
+        <img src="img/logo.png" alt="food-4-oL Logo">
     </section>
 
 </main>
